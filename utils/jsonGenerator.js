d5a14bc5d213aed8c9873ea023f964adcb2ccaaa
Elie-A <elieabdallah961@gmail.com>
2024-03-08 22:01:32 +0100

    Fixing regex generaation, added enum

--- utils/jsonGenerator.js
+++ utils/jsonGenerator.js
@@ -1,49 +1,42 @@
 const crypto = require('crypto');
+const RandExp = require('randexp');
 
-function parseLengthFromPattern(pattern) {
-    // Extracts the length information from a regex pattern
-    const match = pattern.match(/\{(\d+)(?:,(\d+))?\}/);
-    if (match) {
-        const [, first, second] = match;
-        const toReturn = second ? [parseInt(first), parseInt(second)] : [parseInt(first)];
-        return toReturn;
-    }
-    return undefined;
-}
+const ALPHA_NUM = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-';
 
 function generateDataForProperty(propertySchema, propertyPath) {
     const type = propertySchema.type;
     let length;
-    let parsedLengthArray;
+    let regex;
 
     if (type === 'integer') {
         const min = propertySchema.minimum || 0;
         const max = propertySchema.maximum || Number.MAX_VALUE;
         return parseInt(Math.random() * (max - min + 1)) + min;
     } else if (type === 'string') {
+        const format = propertySchema.format;
         const pattern = propertySchema.pattern;
-        if(pattern){
-            parsedLengthArray = parseLengthFromPattern(pattern);
-            if (parsedLengthArray.length === 1) {
-                length = parsedLengthArray[0];
-            } else if (parsedLengthArray.length > 1) {
-                length = Math.floor(Math.random() * (parsedLengthArray[1] - parsedLengthArray[0] + 1)) + parsedLengthArray[0];
-            }
-        } else {
-            length = propertySchema.length || 10; // Default length if not provided in the pattern or schema
-        }
 
-        const regex = pattern ? new RegExp(pattern) : undefined;
-
+        if (format && !propertySchema.useDefault) {
+            return generateRandomValueFromFormat(format);
+        } else if (!format && pattern) {
+            regex = pattern ? new RegExp(pattern) : undefined;
+        } 
         if (regex) {
             let generatedString;
             do {
-                generatedString = generateRandomString(length);
+                const randExp = new RandExp(regex);
+                generatedString = randExp.gen();
             } while (!regex.test(generatedString));
 
             return generatedString;
         } else {
-            return propertySchema.useDefault && propertySchema.default ? propertySchema.default : generateRandomString(length);
+            // Check if the property has an enum definition
+            if (propertySchema.enum && Array.isArray(propertySchema.enum) && propertySchema.enum.length > 0) {
+                // Select a random value from the enum list
+                return propertySchema.enum[Math.floor(Math.random() * propertySchema.enum.length)];
+            }
+            length = propertySchema.length || 10; // Default length if not provided in the pattern or schema
+            return propertySchema.useDefault && propertySchema.default ? propertySchema.default : generateRandomValue(length);
         }
     } else if (type === 'object') {
         const properties = {};
@@ -53,13 +46,18 @@ function generateDataForProperty(propertySchema, propertyPath) {
         }
         return properties;
     } else if (type === 'array') {
-        const items = [];
-        const itemType = propertySchema.items.type;
+        const generatedItems = [];
+        const items = propertySchema.items;
+
         for (let i = 0; i < items.length; i++) {
-            const itemData = generateDataForProperty(itemType, propertyPath.concat(['items']));
-            items.push(itemData);
+            const itemData = generateDataForProperty(items[i], propertyPath.concat(['items', i.toString()]));
+            generatedItems.push(itemData);
         }
-        return items;
+        return generatedItems;
+    } else if (type === 'boolean') {
+        const randomBytes = crypto.randomBytes(1);
+        const randomBit = randomBytes[0] & 1; // Extract the least significant bit
+        return Boolean(randomBit);
     } else {
         // For types other than integer, string, object, and array
         const useDefault = propertySchema.useDefault || false;
@@ -68,16 +66,33 @@ function generateDataForProperty(propertySchema, propertyPath) {
     }
 }
 
-function generateRandomString(length) {
+function generateRandomValue(length) {
     const randomBytes = crypto.randomBytes(length);
-    const ALPHA_NUM = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-    let result = '';
+    let generatedValue = '';
     for (let i = 0; i < length; i++) {
         const index = randomBytes[i] % ALPHA_NUM.length;
-        result += ALPHA_NUM.charAt(index);
+        generatedValue += ALPHA_NUM.charAt(index);
     }
 
-    return result;
+    return generatedValue;
+}
+
+function generateRandomValueFromFormat(format){
+    let generatedValueFromFormat = '';
+    switch (format) {
+        case 'email':
+            generatedValueFromFormat = generateRandomEmail();
+            return generatedValueFromFormat;
+        default:
+            return generatedValueFromFormat;
+    }
+}
+
+function generateRandomEmail(){
+    const usernameLength = 8;
+    const username = generateRandomValue(usernameLength);
+    const domain = '@example.com';
+    return `${username}${domain}`;
 }
 
 module.exports = {
