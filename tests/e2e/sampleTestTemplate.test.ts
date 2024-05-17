59e65d4882acb9a56559c81a56813a2148d202c0
Elie-A <elieabdallah961@gmail.com>
2024-05-17 16:25:05 +0200

    Adding LICENSE, readme

--- tests/e2e/sampleTestTemplate.test.ts
+++ tests/e2e/sampleTestTemplate.test.ts
@@ -1,15 +1,15 @@
-import { readAndSaveSchema, generateJsonFromSchema } from '../../index';
+import { readAndSaveSchema, generateJsonFromSchema } from "../../index";
 
-const templatePath = './templates/sampleTestTemplate.json';
+const templatePath = "./templates/sampleTestTemplate.json";
 
-test('generateJsonFromSchema should generate valid JSON data', async () => {
+test("generateJsonFromSchema should generate valid JSON data", async () => {
   const propertySchema = await readAndSaveSchema(templatePath);
 
   if (propertySchema !== null) {
-    const generatedJson = generateJsonFromSchema(propertySchema, ['root']);
+    const generatedJson = generateJsonFromSchema(propertySchema, ["root"]);
     // console.log(JSON.stringify(generatedJson, null, 2));
-    expect(typeof generatedJson).toBe('object');
+    expect(typeof generatedJson).toBe("object");
   } else {
-    fail('Failed to load property schema');
+    fail("Failed to load property schema");
   }
 });
